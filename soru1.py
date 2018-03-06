cebbdc085b9e18052b731f8032b230444ca85a73
hukumrancelik <36935662+hukumrancelik@users.noreply.github.com>
2018-03-06 22:30:41 +0300

    Update soru1.py

--- soru1.py
+++ soru1.py
@@ -1,10 +1,22 @@
-print("'adamCiro()' fonksiyonunu giriniz")
-def adamCiro():
-    print("***TOPLAM CİRO***")
-    satisMik=int(input("Satış Miktarını Giriniz:"))
-    satisFiyat=int(input("Satış Fiyatını Giriniz:"))
-    ciroToplam=satisMik*satisFiyat
-    print("Toplam Ciro:",ciroToplam)
-    ciroAdam=ciroToplam/25
-    print("***ADAMBAŞI CİRO**")
-    print("Adambaşı Ciro:",ciroAdam)
+print("***GELİRLER***")
+finansGelir=int(input("Finansman Gelirlerini Giriniz:"))
+pazarGelir=int(input("Pazar Gelirlerini Giriniz:"))
+kiraGelir=int(input("Finansman Gelirlerini Giriniz:"))
+toplamGelir=finansGelir+pazarGelir+kiraGelir
+
+print("***GİDERLER***")
+ucret=int(input("Ücret Gelirlerini Giriniz:"))
+finansGider=int(input("Finansman Giderlerini Giriniz:"))
+pazarGider=int(input("Pazar Giderlerini Giriniz:"))
+kiraGider=int(input("Kira Giderlerini Giriniz:"))
+muhasebeGider=int(input("Muhasebe Giderlerini Giriniz:"))
+toplamGider=ucret+finansGider+pazarGider+kiraGider+muhasebeGider
+kar=toplamGelir-toplamGider
+
+if kar==0:
+   print(kar,"TL İLE İŞLETME BAŞABAŞ NOKTASINDADIR")
+
+elif kar<1000:
+    print(kar,"TL İLE İŞLETME ZARARDADIR")
+else:
+    print(kar,"TL İLE İŞLETME KAR NOKTASINDADIR")
