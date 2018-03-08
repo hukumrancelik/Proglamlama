c04a7466020a34d5768e7d1d25f2c70fc39bfbae
hukumrancelik <36935662+hukumrancelik@users.noreply.github.com>
2018-03-08 16:33:34 +0300

    Update soru2.py

--- soru2.py
+++ soru2.py
@@ -1,40 +1,38 @@
-print("----------------------------------")
-print("KULLANACAĞINIZ FONKSİYONU YAZINIZ")
-print("----------------------------------")
-print("EKİPMAN KULLANABİLİRLİK için:'ekipmanKullan()'")
-print("PERFORMANS ORANI için:'performansOrani()'")
-print("KALİTE ORANI için:'kalite()'")
-print("EKİPMAN ETKİNLİK ORANI için:'etkinlik()'")
-
-def ekipmanKullan():
-    print("***EKİPMAN KULLANABİLİRLİK***")
-    planUretimSur=int(input("Planlamış Üretim Süresini Giriniz:"))
-    plansizUretimSur=int(input("Plansız Üretim Süresini Giriniz:"))
+def ekipmanKullan( planUretimSur, plansizUretimSur):
+    global kullanabilirlik
     kullanabilirlik=(planUretimSur-plansizUretimSur)/planUretimSur
-    print("Kullanabilirlik:",kullanabilirlik)
+    return kullanabilirlik
 
-def performansOrani():
-    print("***PERFORMANS ORANI***")
-    standCevSur=int(input("Standart Cevrim Süresini Giriniz:"))
-    uretimMik=int(input("Üretim Miktarını Giriniz:"))
-    planUretimSur=int(input("Planlamış Üretim Süresini Giriniz:"))
-    plansizUretimSur=int(input("Plansız Üretim Süresini Giriniz:"))
+def performansOrani( standCevSur,uretimMik,):
     performans=(standCevSur*uretimMik)/(planUretimSur-plansizUretimSur)
-    print("Kullanabilirlik:",performans)
+    return performans
     
-def kalite():
-    print("***KALİTE ORANI***")
-    saglamUrun=int(input("Sağlam Urun Miktarını Giriniz:"))
-    toplamUrun=int(input("Toplam ürün Miktarını Giriniz:"))
+def kaliteHesap(saglamUrun,toplamUrun):
+    global kalite
     kalite=saglamUrun/toplamUrun
-    print("Kalite Oranı:",kalite)
-   
+    return kalite
+    
+def etkinlik(kullanabilirlik,performans,kalite):
+    oee=kullanabilirlik*performans*kalite*100
 
-def etkinlik():
-    print("***EKİPMAN ETKİNLİK ORANI***")
-    kullanabilirlik=float(input("Kullanabilirlik Oranın Giriniz:"))
-    performans=float(input("Performans Oranını Giriniz:"))
-    kalite=float(input("Kalite Oranını Giriniz:"))
-    oee=(kullanabilirlik*performans*kalite)*100
-    print("Ekipman Etkinlik Oranı",oee)
-   
+    if(oee>75):
+        print("Verimlilik İyidir")
+    elif(50<oee<75):
+        print("Verimlilik Normal")
+    else:
+        print("Verimlilik Kötü")
+
+planUretimSur=int(input("Planlamış Üretim Süresini Giriniz:"))
+plansizUretimSur=int(input("Plansız Üretim Süresini Giriniz:"))
+
+standCevSur=int(input("Standart Cevrim Süresini Giriniz:"))
+uretimMik=int(input("Üretim Miktarını Giriniz:"))
+
+saglamUrun=int(input("Sağlam Urun Miktarını Giriniz:"))
+toplamUrun=int(input("Toplam ürün Miktarını Giriniz:"))
+
+k=ekipmanKullan( planUretimSur, plansizUretimSur)
+p=performansOrani( standCevSur,uretimMik,)
+etkinlik(kullanabilirlik,performans,kalite)
+
+
