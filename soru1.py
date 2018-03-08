c2698accbfb1b99b4f4bc09d1d4acc3a20a3599f
hukumrancelik <36935662+hukumrancelik@users.noreply.github.com>
2018-03-08 16:32:17 +0300

    Update soru1.py

--- soru1.py
+++ soru1.py
@@ -1,8 +1,25 @@
+def gelirHesapla(finansGelir,pazarGelir,kiraGelir):
+    global toplamGelir
+    toplamGelir=finansGelir+pazarGelir+kiraGelir
+    return toplamGelir
+
+def giderHesapla(ucret,finansGider,pazarGider,kiraGider,muhasebeGider):
+    global toplamGider
+    toplamGider=ucret+finansGider+pazarGider+kiraGider+muhasebeGider
+    return toplamGider
+def karHesapla(toplamGelir,toplamGider):
+    kar=toplamGelir-toplamGider
+    if (kar>1000):
+        print(kar,"TL İLE İŞLETME KAR NOKTASINDADIR")
+    elif kar<1000:
+        print(kar,"TL İLE İŞLETME ZARARDADIR")
+    else:
+        print(kar,"İŞLETMENİNİZ BAŞABAŞ NOKTASINDADIR")
+
 print("***GELİRLER***")
 finansGelir=int(input("Finansman Gelirlerini Giriniz:"))
 pazarGelir=int(input("Pazar Gelirlerini Giriniz:"))
 kiraGelir=int(input("Finansman Gelirlerini Giriniz:"))
-toplamGelir=finansGelir+pazarGelir+kiraGelir
 
 print("***GİDERLER***")
 ucret=int(input("Ücret Gelirlerini Giriniz:"))
@@ -10,13 +27,14 @@ finansGider=int(input("Finansman Giderlerini Giriniz:"))
 pazarGider=int(input("Pazar Giderlerini Giriniz:"))
 kiraGider=int(input("Kira Giderlerini Giriniz:"))
 muhasebeGider=int(input("Muhasebe Giderlerini Giriniz:"))
-toplamGider=ucret+finansGider+pazarGider+kiraGider+muhasebeGider
-kar=toplamGelir-toplamGider
 
-if kar==0:
-   print(kar,"TL İLE İŞLETME BAŞABAŞ NOKTASINDADIR")
+gelirim=gelirHesapla(finansGelir,pazarGelir,kiraGelir)
+giderim=giderHesapla(ucret,finansGider,pazarGider,kiraGider,muhasebeGider)
+kar=karHesapla(gelirim,giderim)
 
-elif kar<1000:
-    print(kar,"TL İLE İŞLETME ZARARDADIR")
-else:
-    print(kar,"TL İLE İŞLETME KAR NOKTASINDADIR")
+
+
+
+
+
+
